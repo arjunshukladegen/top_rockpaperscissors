497aa6c47495e6d2ea932eee53ff04f1ac6848ae
arjun <arjunshukladegen@gmail.com>
2026-07-09 16:27:22 -0700

    setting up pseudocode

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+getComputerChoice FUNC
+  return rock paper or scissors at random
+
+getHumanChoice FUNC
+  prompt user for choice
+
+let humanScore track score
+let computerScore track score
+
+playRound FUNC (humanChoice and computerChoice)
+  make human choice not case sensitive
+  console log a round winner such as "You lose! Paper beats Rock"
+  increment score based on winner 
+
+playGame FUNC
+  calls playRound to play 5 rounds
+
+*/
