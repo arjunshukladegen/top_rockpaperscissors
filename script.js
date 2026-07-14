8abed74b906b91e59f86fe0775d5e30b37fbfff1
arjun <arjunshukladegen@gmail.com>
2026-07-14 16:14:34 -0700

    general updates

--- script.js
+++ script.js
@@ -18,10 +18,7 @@ function getComputerChoice() {
 
 
 // Prompts user for them to type rock, paper, or scissors
-function getHumanChoice() {
-  let playerPick = prompt("Pick 'Rock', 'Paper', or 'Scissors'!");
-  return playerPick;
-}
+
 //console.log(`You picked: ${getHumanChoice()}`);
 
 
@@ -33,99 +30,231 @@ function getHumanChoice() {
 
 
 // Plays a 5 round game
-function playGame() {
-  // Track scores for game
-  let humanScore = 0;
-  let computerScore = 0;
-  let roundWinner;
+// function playGame() {
+  
 
-  // Get human and computer choices and calculates winner
-  function playRound(humanChoice, computerChoice) {
-    console.log(`You have picked: ${humanChoice}`);
-    console.log(`Computer has picked: ${computerChoice}`);
+//   // Get human and computer choices and calculates winner
+//   function playRound(humanChoice, computerChoice) {
+//     console.log(`You have picked: ${humanChoice}`);
+//     console.log(`Computer has picked: ${computerChoice}`);
 
-    let winnerName;
+//     let winnerName;
 
-    // Calculate who the winner is
+//     // Calculate who the winner is
 
-    // If they picked the same (tie)
-    if (humanChoice === computerChoice) {
-      winnerName = "Tie";
-    }
+//     // If they picked the same (tie)
+//     if (humanChoice === computerChoice) {
+//       winnerName = "Tie";
+//     }
 
-    // If human picked Rock
-    else if (humanChoice === "rock") {
-      if (computerChoice === "paper") {
-        winnerName = "Computer";
-      } 
-      else {
-        winnerName = "Human"
-      }
-    }
+//     // If human picked Rock
+//     else if (humanChoice === "rock") {
+//       if (computerChoice === "paper") {
+//         winnerName = "Computer";
+//       } 
+//       else {
+//         winnerName = "Human"
+//       }
+//     }
 
-    // If human picked Paper
-    else if (humanChoice === "paper") {
-      if (computerChoice === "scissors") {
-        winnerName = "Computer";
-      } 
-      else {
-        winnerName = "Human"
-      }
-    }
+//     // If human picked Paper
+//     else if (humanChoice === "paper") {
+//       if (computerChoice === "scissors") {
+//         winnerName = "Computer";
+//       } 
+//       else {
+//         winnerName = "Human"
+//       }
+//     }
 
-    // If human picked Scissors
-    else if (humanChoice === "scissors") {
-      if (computerChoice === "rock") {
-        winnerName = "Computer";
-      } 
-      else {
-        winnerName = "Human"
-      }
-    }
-    if (winnerName === "Tie") {
-      console.log("This round is a Tie!");
-    }
-    else {
-      console.log(`${winnerName} has won this round!`);
-    }
-    return winnerName;
-  }
+//     // If human picked Scissors
+//     else if (humanChoice === "scissors") {
+//       if (computerChoice === "rock") {
+//         winnerName = "Computer";
+//       } 
+//       else {
+//         winnerName = "Human"
+//       }
+//     }
+//     if (winnerName === "Tie") {
+//       console.log("This round is a Tie!");
+//     }
+//     else {
+//       console.log(`${winnerName} has won this round!`);
+//     }
+//     return winnerName;
+//   }
 
-  for (let i = 0; i < 5; i++) {
-    console.log(`Round ${i+1} has Started!`);
-    let humanSelection = getHumanChoice().toLowerCase();
-    let computerSelection = getComputerChoice();
+//   for (let i = 0; i < 5; i++) {
+//     console.log(`Round ${i+1} has Started!`);
+//     let humanSelection = getHumanChoice().toLowerCase();
+//     let computerSelection = getComputerChoice();
 
-    roundWinner = playRound(humanSelection, computerSelection);
-    if (roundWinner === 'Computer') {
-      computerScore = computerScore + 1;
-    }
-    else if (roundWinner === 'Human') {
-      humanScore = humanScore + 1;
-    }
+//     roundWinner = playRound(humanSelection, computerSelection);
+//     if (roundWinner === 'Computer') {
+//       computerScore = computerScore + 1;
+//     }
+//     else if (roundWinner === 'Human') {
+//       humanScore = humanScore + 1;
+//     }
 
-    console.log(`Round ${i+1} has Ended!`)
-    console.log(`Computer: ${computerScore} | Human ${humanScore}`)
-  }
-  let whoWon;
-  if (computerScore > humanScore) {
-    whoWon = "The Computer"
-  }
-  else if (humanScore > computerScore) {
-    whoWon = "The Human"
-  }
-  else {
-    whoWon = "No one"
-  }
-  return `Game has Ended!!! ${whoWon} has won the game!`;
-}
+//     console.log(`Round ${i+1} has Ended!`)
+//     console.log(`Computer: ${computerScore} | Human ${humanScore}`)
+//   }
+//   let whoWon;
+//   if (computerScore > humanScore) {
+//     whoWon = "The Computer"
+//   }
+//   else if (humanScore > computerScore) {
+//     whoWon = "The Human"
+//   }
+//   else {
+//     whoWon = "No one"
+//   }
+//   return `Game has Ended!!! ${whoWon} has won the game!`;
+// }
+
+
+
 
 // playGame() - Starts 5 round game
 // DOM Manipulation
-const button = document.querySelector('#start-game');
 
-button.addEventListener('click', function (e) {
-  console.log("Hi");
+
+
+
+
+function startGame() {
+
+  // VARIABLE DECLARATION
+
+  let humanScore = 0;
+  let computerScore = 0;
+  let humanPick;
+  let computerPick;
+  let roundWinner;
+
+  // DOM SELECTORS
+
+  // IMAGE FOR THE HUMAN'S CURRENT PICK
+  const humanPickImage = document.querySelector("#user-pick-image");
+  humanPickImage.src = 'img/eye.png';
+
+  // IMAGE FOR THE COMPUTER'S CURRENT PICK
+  const computerPickImage = document.querySelector("#computer-pick-image");
+  computerPickImage.src = 'img/eye.png';
+
+  // BOTTOM MIDDLE UI SCORE TEXT
+  const middleScore = document.querySelector("#middle-score");
+  middleScore.textContent = `Tie`;
+
+  // HUMAN SCORE TRACKER
+  const humanScoreText = document.querySelector('#human-score');
+  humanScoreText.textContent = `Score: ${humanScore}`;
+  // COMPUTER SCORE TRACKER
+  const computerScoreText = document.querySelector('#computer-score');
+  computerScoreText.textContent = `Score: ${computerScore}`;
+
+
+
+
+  // CODE GOES UNDER HERE
+  
+}
+
+
+// FILE STARTS HERE
+// ----------------------------------------------------------
+
+let humanScore = 0;
+let computerScore = 0;
+let humanPick;
+let computerPick;
+let roundWinner;
+
+// DOM SELECTORS
+
+// IMAGE FOR THE HUMAN'S CURRENT PICK
+const humanPickImage = document.querySelector("#user-pick-image");
+humanPickImage.src = 'img/eye.png';
+
+
+// IMAGE FOR THE COMPUTER'S CURRENT PICK
+const computerPickImage = document.querySelector("#computer-pick-image");
+computerPickImage.src = 'img/eye.png';
+
+
+// BOTTOM MIDDLE UI SCORE TEXT
+const middleScore = document.querySelector("#middle-score");
+middleScore.textContent = `Round 0`;
+
+// HUMAN SCORE TRACKER
+const humanScoreText = document.querySelector('#human-score');
+humanScoreText.textContent = `Score: ${humanScore}`;
+// COMPUTER SCORE TRACKER
+const computerScoreText = document.querySelector('#computer-score');
+computerScoreText.textContent = `Score: ${computerScore}`;
+
+
+
+
+
+
+function humanChoice(choice) {
+  if (choice === 'rock') {
+    humanPickImage.src = 'img/rock.png';
+    humanPickImage.style.borderColor = "#FFFF00";
+  }
+  else if (choice === 'paper') {
+    humanPickImage.src = 'img/paper.png';
+    humanPickImage.style.borderColor = "#FFFF00";
+  }
+  else if (choice === 'scissors') {
+    humanPickImage.src = 'img/scissors.png';
+    humanPickImage.style.borderColor = "#FFFF00";
+  }
+  else {
+    console.log("Error: Something went wrong, please restart.");
+  }
+  }
+
+const rockButton = document.querySelector('#rock-button');
+rockButton.addEventListener('click', function (e) {
+  console.log("admin: rock selected");
+  humanChoice('rock');
+})
+
+const paperButton = document.querySelector('#paper-button');
+paperButton.addEventListener('click', function (e) {
+  console.log("admin: paper selected");
+  humanChoice('paper');
+})
+
+const scissorsButton = document.querySelector('#scissors-button');
+scissorsButton.addEventListener('click', function (e) {
+  console.log("admin: scissors selected");
+  humanChoice('scissors');
 })
 
 
+let gameStarted = false;
+
+// START THE GAME
+const mainButton = document.querySelector('#start-game');
+mainButton.textContent = 'Start Game';
+mainButton.style.backgroundColor = "#FBB942";
+mainButton.addEventListener('click', function (e) {
+  console.log("admin: main button has been clicked");
+  if (gameStarted === false) {
+    gameStarted = true;
+    mainButton.style.backgroundColor = "#FFFF00";
+    humanPickImage.style.borderColor = "#FF0000";
+    mainButton.textContent = 'Lock In';
+    startGame();
+  }
+  else {
+
+  }
+
+  
+})
