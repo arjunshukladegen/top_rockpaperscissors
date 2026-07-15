eb26aa4d5e133bfa5e12703de892aa022c688193
arjun <arjunshukladegen@gmail.com>
2026-07-14 18:44:28 -0700

    setup new js file i will be switching to

---
+++ alt.js
@@ -0,0 +1,15 @@
+const state = {
+  round: 0, // the round number
+  cpuScore: 0, // the computer's score
+  playerScore: 0, // the player's score
+  cpuSelection: null, // move selected by ai at random 
+  playerSelection: null, // the move that player is hovering
+  cpuName: 'Computer', // name that appears for cpu
+  playerName: 'Human', // name that appears for player
+  gameStarted: false, // (checks if game has started)
+  roundWinner: null, // (cpu, human, or tie)
+}
+
+const ui = {
+  
+}
